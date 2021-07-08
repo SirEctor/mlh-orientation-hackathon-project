865819fceca8d68c4034128514d27dd8518bcd82
SirEctor <ebenezer.teshome@duke.edu>
2021-07-08 13:10:34 -0400

    prodtest changed

--- prod-test.sh
+++ prod-test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 echo "testing 1 2 3..."
 curl "etportfolio.duckdns.org/health/"
-curl -X POST -d "username=testUsername" "etportfolio.duckdns.org/register" -L
-curl -X POST -d "username=testPassword" "etportfolio.duckdns.org/register" -L
-curl -X POST -d "username=testUsername&password=testPassword" "etportfolio.duckdns.org/register" -L
-curl -X POST -d "username=testUsername" "etportfolio.duckdns.org/login" -L
-curl -X POST -d "password=testPassword" "etportfolio.duckdns.org/login" -L
-curl -X POST -d "username=testUsername&password=testPassword" "etportfolio.duckdns.org/login" -L
+curl -X POST -d "username=testUsername" "https://etportfolio.duckdns.org/register" -L
+curl -X POST -d "username=testPassword" "https://etportfolio.duckdns.org/register" -L
+curl -X POST -d "username=testUsername&password=testPassword" "https://etportfolio.duckdns.org/register" -L
+curl -X POST -d "username=testUsername" "https://etportfolio.duckdns.org/login" -L
+curl -X POST -d "password=testPassword" "https://etportfolio.duckdns.org/login" -L
+curl -X POST -d "username=testUsername&password=testPassword" "https://etportfolio.duckdns.org/login" -L
